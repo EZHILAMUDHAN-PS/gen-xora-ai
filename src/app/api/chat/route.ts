62fb0260197ec089f411cc1c4e7d50e9f8370d41
EZHILAMUDHAN-PS <240706.EV@rmkec.ac.in>
2026-06-21 11:34:23 +0530

    change model

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -10,7 +10,7 @@ export async function POST(req: Request) {
     });
 
     const response = await ai.models.generateContent({
-      model: "gemini-2.5-flash",
+      model: "gemini-2.0-flash",
       contents: message,
     });
 
